dd614e26d00b0b127e4faf860821b740109b088f
Phuong <lephuong10142002@gmail.com>
2023-09-04 17:56:44 +0700

    integrated socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 import userRouter from "./routes/users.routes";
 import databaseService from "./database/database";
@@ -15,6 +17,7 @@ import { UPLOAD_VIDEO_DIR } from "./constants/dir";
 
 const app = express();
 const port = process.env.PORT || 8080;
+const httpServer = createServer(app);
 
 //config cors
 app.use(cors());
@@ -33,8 +36,21 @@ app.use("/static/video", express.static(UPLOAD_VIDEO_DIR));
 
 app.use(defaultErrorHandler);
 
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected`);
+  socket.on("disconnect", () => {
+    console.log(`user ${socket.id} disconnected`);
+  });
+});
+
 databaseService.connect();
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
